5dcd630b6e3eaefc4194865957090502608544d5
gabriel2302 <gabriel23002@gmail.com>
2024-04-06 19:46:39 -0300

    feat: ensure CreateTag calls IdGenerator

--- src/data/usecases/tag/create-tag.ts
+++ src/data/usecases/tag/create-tag.ts
@@ -1,11 +1,20 @@
 import { type CreateTagRepository } from '@/data/protocols/repository/tag/create-tag-repository'
+import { type IdGenerator } from '@/data/protocols/services/id-generator'
 import { type CreateTag } from '@/domain/protocols/tag'
 
 export class CreateTagUseCase implements CreateTag {
-  constructor (private readonly tagRepository: CreateTagRepository) {}
+  constructor (
+    private readonly createTagRepository: CreateTagRepository,
+    private readonly idGenerator: IdGenerator
+  ) {}
 
   async create (data: CreateTag.Params): Promise<CreateTag.Result> {
-    const tag = await this.tagRepository.create(data)
+    const skTag = this.idGenerator.generate()
+
+    const tag = await this.createTagRepository.create({
+      sk_tag: skTag,
+      tag_name: data.tag_name
+    })
 
     return {
       sk_tag: tag.sk_tag
