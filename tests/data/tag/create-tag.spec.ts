8ac2a68522ccb870a23a4e82f8dd2b47e32b8ff7
gabriel2302 <gabriel23002@gmail.com>
2024-04-06 19:46:30 -0300

    test: ensure CreateTag calls IdGenerator

--- tests/data/tag/create-tag.spec.ts
+++ tests/data/tag/create-tag.spec.ts
@@ -1,37 +1,54 @@
 import { CreateTagUseCase } from '@/data/usecases/tag/create-tag'
-import { CreateTagRepositorySpy } from '../mocks'
+import { CreateTagRepositorySpy, IdGeneratorSpy } from '../mocks'
 
 type SutTypes = {
   sut: CreateTagUseCase
   createTagRepositorySpy: CreateTagRepositorySpy
+  idGeneratorSpy: IdGeneratorSpy
 }
 
 const makeSut = (): SutTypes => {
   const createTagRepositorySpy = new CreateTagRepositorySpy()
-  const sut = new CreateTagUseCase(createTagRepositorySpy)
+  const idGeneratorSpy = new IdGeneratorSpy()
+
+  const sut = new CreateTagUseCase(createTagRepositorySpy, idGeneratorSpy)
   return {
     sut,
-    createTagRepositorySpy
+    createTagRepositorySpy,
+    idGeneratorSpy
   }
 }
 
 describe('Create Tag Use Case', () => {
+  it('should call IdGenerator', async () => {
+    const { sut, idGeneratorSpy } = makeSut()
+    const generateSpy = jest.spyOn(idGeneratorSpy, 'generate')
+
+    await sut.create({ tag_name: 'any_name' })
+
+    expect(generateSpy).toHaveBeenCalled()
+  })
+
   it('should call CreateTagRepository with correct values', async () => {
-    const { sut, createTagRepositorySpy } = makeSut()
+    const { sut, createTagRepositorySpy, idGeneratorSpy } = makeSut()
 
     const params = {
-      tag_name: 'any_name',
-      sk_tag: 'any_sk_tag'
+      tag_name: 'any_name'
     }
 
+    idGeneratorSpy.result = 'any_sk_tag'
+
     await sut.create(params)
-    expect(createTagRepositorySpy.params).toEqual(params)
+    expect(createTagRepositorySpy.params).toEqual({
+      sk_tag: 'any_sk_tag',
+      tag_name: 'any_name'
+    })
   })
 
   it('should throw if CreateTagRepository throws', async () => {
     const { sut, createTagRepositorySpy } = makeSut()
     jest.spyOn(createTagRepositorySpy, 'create').mockRejectedValueOnce(new Error())
-    const promise = sut.create({ tag_name: 'any_name', sk_tag: 'any_sk_tag' })
+    const promise = sut.create({ tag_name: 'any_name' })
     await expect(promise).rejects.toThrow()
   })
 })
